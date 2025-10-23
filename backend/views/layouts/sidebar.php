eb75da948b2418eccdd1d5360a0e87c156024a34
GS1-Hub <guilhermesa017@gmail.com>
2025-10-23 23:29:53 +0100

    Começo das Gestão de Users

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src="<?=$assetDir?>../web/assets/img/VortexApp_Logo-NoBackground.png" alt="Vortex Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">BackEnd Painel</span>
     </a>
 
     <!-- Sidebar -->
