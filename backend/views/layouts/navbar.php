ce6a7506f4acfc0ee87868ae4ec541679612a7cd
GS1-Hub <guilhermesa017@gmail.com>
2025-10-23 23:43:43 +0100

    Update navbar.php

--- backend/views/layouts/navbar.php
+++ backend/views/layouts/navbar.php
@@ -6,7 +6,6 @@ use yii\helpers\Html;
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     
-
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <!-- Navbar Search -->
