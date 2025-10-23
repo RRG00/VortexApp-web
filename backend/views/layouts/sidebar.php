dfcb1b3c9e541ecc77920222de8b2c3ca0955ddd
RRG00 <2241553@my.ipleiria.pt>
2025-10-23 23:36:49 +0100

    gitignore para backend
    alteração do gitignore do backend

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="<?=$assetDir?>../web/assets/img/VortexApp_Logo-NoBackground.png" alt="Vortex Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="../web/assets/img/VortexApp_Logo-NoBackground.png" alt="Vortex Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">BackEnd Painel</span>
     </a>
 
