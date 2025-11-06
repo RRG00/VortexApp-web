7f6ac9aa36288db75b44db48d4af212babd5f30b
GS1-Hub <guilhermesa017@gmail.com>
2025-11-06 21:55:52 +0000

    FEAT: Introduce new RBAC roles (Player, Equipa, Referre)

--- console/controllers/RbacController.php
+++ console/controllers/RbacController.php
@@ -13,7 +13,7 @@ class RbacController extends Controller
         // Delete all
         $auth->removeAll();
 
-        // Create role Admin and premissions
+        // Create role Admin
         $admin = $auth->createRole('admin');
         $admin->description = 'Administrador - gere users';
         $auth->add($admin);
@@ -23,12 +23,35 @@ class RbacController extends Controller
         $organizer->description = 'Organizador - gere users';
         $auth->add($organizer);
 
+        //Create Role Player
+        $player = $auth->createRole('player');
+        $player->description = 'Player';
+        $auth->add($player);
+
+        //Create Role Equipa
+        $equipa = $auth->createRole('equipa');
+        $equipa->description = 'Equipa';
+        $auth->add($equipa);
+
+        //Create referee
+        $referre = $auth->createRole('referre');
+        $referre->description = 'Referre';
+        $auth->add($referre);
+
+
         $auth->assign($admin,1);
+        $auth->assign($equipa,2);
+        $auth->assign($referre,3);
         $auth->assign($organizer,4);
+        $auth->assign($player,5);
+
 
 
         echo "RBAC Admin criado com sucesso!\n";
         echo "RBAC Organizador criado com sucesso!\n";
+        echo "RBAC Player criado com sucesso!\n";
+        echo "RBAC Equipa criado com sucesso!\n";
+        echo "RBAC Referre criado com sucesso!\n";
 
 
     }
