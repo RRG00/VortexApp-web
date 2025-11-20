ad5d164b3f067c25709d1194616442dc9c9a6a82
GS1-Hub <guilhermesa017@gmail.com>
2025-11-20 17:22:34 +0000

    referre changes

--- backend/views/referre/index.php
+++ backend/views/referre/index.php
@@ -10,35 +10,33 @@ use yii\grid\GridView;
 /** @var app\models\ReferreSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Referres';
-$this->params['breadcrumbs'][] = $this->title;
+// Importa o CSS externo
+$this->registerCssFile('@web/css/referre-index.css', ['depends' => [\yii\bootstrap4\BootstrapAsset::class]]);
 ?>
-<div class="referre-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Referre', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
+<div class="referre-index-wrapper">
+  <div class="referre-card">
+    <div class="referre-card-header">
+      <?= Html::encode($this->title) ?>
+    </div>
+    <div class="referre-card-body">
+      <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+        'tableOptions' => ['class' => 'referre-table table table-hover align-middle mb-0'],
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'id_partida',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Referre $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
+          ['class' => 'yii\grid\SerialColumn'],
+          'id_partida',
+          [
+            'class' => 'yii\grid\ActionColumn',
+            'header' => 'Ações',
+            'urlCreator' => function ($action, $model, $key, $index, $column) {
+              return Url::toRoute([$action, 'id' => $model->id]);
+            },
+          ],
         ],
-    ]); ?>
-
-
+      ]); ?>
+    </div>
+  </div>
 </div>
+
