77770ea3c6c537dd5760d66fc0e16a4de4463225
GS1-HUB <2241539@my.ipleiria.pt>
2026-01-04 19:22:48 +0000

    Updates login test to use submitForm
    Refactors the login test to use the `submitForm` method for submitting the login form.
    This improves the test's reliability and readability.
    Additionally, the test now verifies that the user is redirected to the correct page
    after successfully logging in.

--- frontend/tests/functional/LoginCest.php
+++ frontend/tests/functional/LoginCest.php
@@ -16,15 +16,18 @@ class LoginCest
      * @param FunctionalTester $I
      */
     public function loginUser(FunctionalTester $I)
-    {   
-        $I->amOnRoute('/site/login');
-        $I->see('Preencha os campos abaixo para iniciar sessão:');
+{
+    $I->amOnRoute('site/login');
+    $I->see('Preencha os campos abaixo para iniciar sessão:');
 
-        $I->fillField('LoginForm[username]', 'player');
-        $I->fillField('LoginForm[password]', '12345678');
-        $I->click('Login');
-        
-        //$I->see('Bem-vindo ao Vortex');
-    }
+    $I->submitForm('#login-form', [
+        'LoginForm[username]' => 'player',
+        'LoginForm[password]' => '12345678',
+    ]);
+
+    $I->see('Bem-vindo ao Vortex');
+}
+
+
 
 }
