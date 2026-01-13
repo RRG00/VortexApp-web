19d52eeeaa842a312ec3dd57204ef1a2c3c54889
GS1-HUB <2241539@my.ipleiria.pt>
2026-01-13 22:58:13 +0000

    Improves API token logging
    Enhances API token-based authentication debugging by logging the token value and associated user ID.
    
    Adds 'info' level logging for the 'api' category and configures a dedicated log file.

--- common/models/User.php
+++ common/models/User.php
@@ -126,11 +126,18 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        return static::findOne([
+        \Yii::info('TOKEN API (param): ' . $token, 'api');
+
+        $user = static::findOne([
             'access_token' => $token,
             'status'       => self::STATUS_ACTIVE,
         ]);
+
+        \Yii::info('TOKEN API (user id): ' . ($user ? $user->id : 'null'), 'api');
+
+        return $user;
     }
+
 
 
     /**
